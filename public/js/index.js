48111af472a50d5909f997703c4b02b981148917
Mohammed Mulazada <thisismoniac@gmail.com>
2017-12-05 20:20:19 +0100

    added send location

--- public/js/index.js
+++ public/js/index.js
@@ -42,3 +42,29 @@ document.querySelector('#message-form').addEventListener('submit', function (e) 
 
 	})
 })
+
+socket.on('newLocationMessage', function (message) {
+	var li = jQuery('<li></li>')
+	var a = jQuery('<a target="_blank">My current location</a>')
+
+	li.text(`${message.from}: `)
+	a.attr('href', message.url)
+	li.append(a)
+	jQuery('#messages').append(li)
+})
+
+
+const locationButton = document.querySelector('#send-location').addEventListener('click', function () {
+	if (!navigator.geolocation) {
+		return alert('Geolocation not supported by your browser.')
+	}
+
+	navigator.geolocation.getCurrentPosition(function (position) {
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		})
+	}), function () {
+		alert('Unable to fetch location.')
+	}
+})
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
-const {generateMessage} = require('./utils/message')
+const {generateMessage, generateLocationMessage} = require('./utils/message')
 
 const app = express()
 const server = http.createServer(app)
@@ -33,6 +33,10 @@ io.on('connection', (socket) => {
 		// 	text: newMsg.test,
 		// 	createdAt: new Date().getTime()
 		// })
+	})
+
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
 	})
 
 	socket.on('disconnect', () => {
